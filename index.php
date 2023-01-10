22569c80c068b724e08210a04f6e161c3e385e20
Panha <panhat938@gmail.com>
2023-01-10 15:45:05 +0700

    re

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    
+<!-- index28:48-->
+<head>
+    <?php include('includes/head.php') ?>
+</head>
+    <body>
+        <!-- Begin Body Wrapper -->
+        <div class="body-wrapper">
+            <!-- Begin Header Area -->
+            <?php include('includes/header.php') ?>
+            <!-- Header Area End Here -->
+            <!-- Begin Slider With Banner Area -->
+            <?php include('includes/slideshow.php') ?>
+            <!-- Slider With Banner Area End Here -->
+            <!-- Begin Product Area -->
+            <?php include('home.php') ?>
+            <!-- Product Area End Here -->
+            <!-- Begin Li's Laptop Product Area -->
+            <?php include('laptop_product.php') ?>
+            <!-- Li's Laptop Product Area End Here -->
+            <!-- Begin Li's TV & Audio Product Area -->
+            <?php include('tv_and_audio.php') ?>
+            <!-- Li's TV & Audio Product Area End Here -->
+            <!-- Begin Li's Static Home Area -->
+            <?php include('includes/banner.php') ?>
+            <!-- Li's Static Home Area End Here -->
+            <!-- Begin Li's Trending Product Area -->
+            <?php include('trending_product.php') ?>
+            <!-- Li's Trending Product Area End Here -->
+            <!-- Begin Li's Trendding Products Area -->
+            <?php include ('best_saller.php') ?>
+            <!-- Li's Trendding Products Area End Here -->
+            <!-- Begin Footer Area -->
+            <?php include('includes/footer.php') ?>
+            <!-- Quick View | Modal Area End Here -->
+        </div>
+        <?php include('script.php') ?>
+    </body>
+
+<!-- index30:23-->
+</html>

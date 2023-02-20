6d59344427b3da4e5d24a94ed1a759a8d030b060
chhith <chhith58cambodia2020@gmail.com>
2023-02-20 13:30:45 +0700

    d

--- wbd.sql
+++ wbd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 19, 2023 at 03:20 PM
+-- Generation Time: Feb 20, 2023 at 06:30 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -17,8 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `wbd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `wbd`;
+--
+-- Database: `wbd`
+--
 
 -- --------------------------------------------------------
 
@@ -122,7 +123,7 @@ CREATE TABLE IF NOT EXISTS `tbl_product` (
   PRIMARY KEY (`prod_id`) USING BTREE,
   KEY `cate_id` (`cate_id`),
   KEY `brand_id` (`brand_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `tbl_product`
@@ -134,7 +135,14 @@ INSERT INTO `tbl_product` (`prod_id`, `prod_name`, `prod_des`, `prod_instock`, `
 (48, 'MSI Titan GT77', '17.3\" UHD 120Hz Gaming Laptop: Intel Core i7-12800HX RTX 3070 Ti 32GB DDR5 1TB NVMe SSD, Thunderbolt 4, USB-Type C, Cooler Boost Titan, Win11 Pro: Core Black 12UGS-009', 25, 2599, 28, 2, '1676785859792.png', '#sadf', 3, 1),
 (49, 'MSI Katana GF66', 'MSI Katana GF66 15.6\" 144Hz FHD Gaming Laptop: Intel Core i7-12650H RTX 3050 Ti 16GB 512GB NVMe SSD, Type-C USB 3.2 Gen 1, High-ResoluTion Audio, Cooler Boost 5, Win11 Home: Black 12UD-436', 25, 788, 28, 2, '1676786010480.png', '#sadf', 4, 1),
 (50, 'MSI Computer GF63', 'MSI Computer GF63, NVIDIA GeForce GTX 1650 Graphics, 15.6\" 8GB 256GB Intel Core i5-10300H X4 2.5GHz Win10, Black', 25, 595, 28, 2, '1676786120130.jpg', '#sadf', 5, 1),
-(51, 'ASUS ROG Strix G15', 'ASUS ROG Strix G15 (2022) Gaming Laptop, 15.6â€ 165Hz IPS Type WQHD Display, NVIDIA GeForce RTX 3060, AMD Ryzen 7 6800H, 16GB DDR5, 1TB PCIe SSD, RGB Keyboard, Windows 11 Home, G513RM-AS71-CA', 25, 1999, 28, 11, '1676818210809.jpg', '#sadf', 6, 1);
+(51, 'ASUS ROG Strix G15', 'ASUS ROG Strix G15 (2022) Gaming Laptop, 15.6â€ 165Hz IPS Type WQHD Display, NVIDIA GeForce RTX 3060, AMD Ryzen 7 6800H, 16GB DDR5, 1TB PCIe SSD, RGB Keyboard, Windows 11 Home, G513RM-AS71-CA', 25, 1999, 28, 11, '1676818210809.jpg', '#sadf', 6, 1),
+(55, 'Asus ROG-Zephyrus M16', 'GeForce RTXâ„¢ 3060 Laptop GPU\r\nWindows 11 Home\r\n12th Gen IntelÂ® Coreâ„¢ i7-12700H\r\nROG Nebula Display\r\n40.64cm(16\"), QHD+ 16:10 (2560 x 1600, WQXGA), Refresh Rate:165Hz\r\n512GB PCIeÂ® 4.0 NVMeâ„¢ M.2 Performance SSD', 20, 2350, 28, 11, '1676863110884.jpg', '#sadf', 7, 1),
+(57, 'Asus ROG Zephyrus G15', 'GeForce RTXâ„¢ 3080 Laptop GPU\r\nWindows 11 Home\r\nAMD Ryzenâ„¢ 9 6900HS\r\n39.62cm(15.6\"), WQHD (2560 x 1440) 16:9, Refresh Rate:165Hz\r\n1TB PCIeÂ® 4.0 NVMeâ„¢ M.2 SSD Supports slots up to max of: 4TB M.2 NVMeâ„¢ PCIeÂ® 4.0 SSD', 15, 1650, 28, 11, '1676864285152.jpg', '#sadf', 9, 1),
+(58, 'Asus ROG Zephyrus G15', 'GeForce RTXâ„¢ 3080 Laptop GPU\r\nWindows 11 Home\r\nAMD Ryzenâ„¢ 9 6900HS\r\n39.62cm(15.6\"), WQHD (2560 x 1440) 16:9, Refresh Rate:165Hz\r\n1TB PCIeÂ® 4.0 NVMeâ„¢ M.2 SSD Supports slots up to max of: 4TB M.2 NVMeâ„¢ PCIeÂ® 4.0 SSD', 15, 1350, 28, 11, '1676864457737.jpg', '#sadf', 10, 1),
+(59, 'Asus ROG Zephyrus G14', 'AMD Radeonâ„¢ RX 6700S\r\nWindows 11 Home\r\nAMD Ryzenâ„¢ 9 6900HS\r\nROG Nebula Display\r\n35.56cm(14\"), QHD+ 16:10 (2560 x 1600, WQXGA), Refresh Rate:120Hz\r\n1TB PCIeÂ® 4.0 NVMeâ„¢ M.2 SSD Supports slots up to max of: 4TB M.2 NVMeâ„¢ PCIeÂ® 4.0 SSD', 15, 1299, 28, 11, '1676864586184.jpg', '#sadf', 11, 1),
+(60, 'Asus ROG Zephyrus G15', 'AMD Radeonâ„¢ RX 6700S\r\nWindows 11 Home\r\nAMD Ryzenâ„¢ 9 6900HS\r\nROG Nebula Display\r\n35.56cm(14\"), QHD+ 16:10 (2560 x 1600, WQXGA), Refresh Rate:120Hz\r\n1TB PCIeÂ® 4.0 NVMeâ„¢ M.2 SSD Supports slots up to max of: 4TB M.2 NVMeâ„¢ PCIeÂ® 4.0 SSD', 12, 1500, 28, 11, '1676864664395.jpg', '#sadf', 11, 1),
+(61, 'Lenovo Legion 5 15 Gaming Laptop', 'Brand	Lenovo\r\nSeries	Lenovo Ryzen 3\r\nScreen Size	15.6 Inches\r\nHard Disk Size	1 TB\r\nCPU Model	AMD Ryzen 7 5800H\r\nRam Memory Installed Size	32 GB', 7, 1350, 28, 12, '1676864842787.jpg', '#sadf', 12, 1),
+(62, 'Lenovo Legion 5 Pro', 'nv883f79', 1, 2599, 28, 12, '1676865949757.jpg', '#sadf', 13, 1);
 
 -- --------------------------------------------------------
 

fb62b4699d1ddd12cd7b9508113699435b4e41a1
devit2000 <ditway1405@gmail.com>
2023-02-23 04:54:11 +0700

    x

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <nav>
-    <li><a href="shop-left-sidebar.html">Home</a></li>
-    <li><a href="about-us.html">About Us</a></li>
-    <li><a href="contact.html">Contact</a></li>
-    <li><a href="shop-left-sidebar.html">Smartwatch</a></li>
-    <li><a href="shop-left-sidebar.html">Accessories</a></li>
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+    </ul>
 </nav>

16af7864a74dc5fde061277788728e88852fdc0a
devit2000 <ditway1405@gmail.com>
2023-02-24 22:44:17 +0700

    select product done

--- shop.php
+++ shop.php
@@ -1,3 +1,45 @@
+<?php
+$sql = "all";
+$current = "All Products";
+$sql = "select p.*, c.cate_name, b.brand_name from tbl_product as p inner join tbl_category as c on p.cate_id=c.cate_id inner join tbl_brand as b on p.brand_id=b.brand_id order by prod_name asc";
+if (isset($_POST['choice'])) {
+    $sql = "select p.*, c.cate_name, b.brand_name from tbl_product as p inner join tbl_category as c on p.cate_id=c.cate_id inner join tbl_brand as b on p.brand_id=b.brand_id order by";
+    $c = $_POST['choice'];
+    switch ($c) {
+        case 'all':
+            $sql .= " prod_name asc";
+            $current = "All Products";
+            break;
+        case 'b-a-z':
+            $sql .= " brand_name asc";
+            $current = "Brand (A - Z)";
+            break;
+        case 'b-z-a':
+            $sql .= " brand_name desc";
+            $current = "Brand (Z - A)";
+            break;
+        case 'c-a-z':
+            $sql .= " cate_name asc";
+            $current = "Category (A - Z)";
+            break;
+        case 'c-z-a':
+            $sql .= " cate_name desc";
+            $current = "Category (Z - A)";
+            break;
+        case 'l-h':
+            $sql .= " prod_price asc";
+            $current = "Price Low to High";
+            break;
+        case 'h-l':
+            $sql .= " prod_price desc";
+            $current = "Price High to Low";
+            break;
+    }
+}
+$sql .= " limit 12";
+$result = mysqli_query($conn, $sql);
+
+?>
 <div class="breadcrumb-area">
     <div class="container">
         <div class="breadcrumb-content">
@@ -14,42 +56,27 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <!-- Begin Li's Banner Area -->
-                <div class="single-banner shop-page-banner">
-                    <a href="#">
-                        <img src="images/bg-banner/2.jpg" alt="Li's Static Banner">
-                    </a>
-                </div>
-                <!-- Li's Banner Area End Here -->
-                <!-- shop-top-bar start -->
                 <div class="shop-top-bar mt-30">
                     <div class="shop-bar-inner">
-                        <div class="product-view-mode">
-                            <!-- shop-item-filter-list start -->
-                            <ul class="nav shop-item-filter-list" role="tablist">
-                                <li class="active" role="presentation"><a aria-selected="true" class="active show" data-toggle="tab" role="tab" aria-controls="grid-view" href="#grid-view"><i class="fa fa-th"></i></a></li>
-                                <li role="presentation"><a data-toggle="tab" role="tab" aria-controls="list-view" href="#list-view"><i class="fa fa-th-list"></i></a></li>
-                            </ul>
-                            <!-- shop-item-filter-list end -->
-                        </div>
                         <div class="toolbar-amount">
-                            <span>Showing 1 to 9 of 15</span>
+                            <span><?= $current ?></span>
                         </div>
                     </div>
                     <!-- product-select-box start -->
                     <div class="product-select-box">
                         <div class="product-short">
-                            <p>Sort By:</p>
-                            <form action="index.php?p=shop" method='POST'>
-                                <select class="nice-select" name="choice">
-                                    <option value="all" selected>Relevance</option>
+                            <label for="choice" style="margin-top:10px; margin-right:10px">Sort By:</label>
+                            <form action="index.php?p=shop" method='POST' style="display:flex;gap:15px">
+                                <select class="form-select" name="choice">
+                                    <option value="all" selected>All Product</option>
                                     <option value="b-a-z">Brand (A - Z)</option>
                                     <option value="b-z-a">Brand (Z - A)</option>
                                     <option value="l-h">Price (Low &gt; High)</option>
+                                    <option value="h-l">Price (High &gt; Low)</option>
                                     <option value="c-a-z">Category (A - Z)</option>
                                     <option value="c-z-a">Category (Z - A)</option>
                                 </select>
-                                <button>Go</button>
+                                <button class="btn btn-primary">Go</button>
                             </form>
                         </div>
                     </div>
@@ -63,36 +90,6 @@
                             <div class="product-area shop-product-area">
                                 <div class="row">
                                     <?php
-                                    $sql = "all";
-                                    $sql = "select * from tbl_product";
-                                    if (isset($_POST['choice'])) {
-                                        $c = $_POST['choice'];
-                                        switch ($c) {
-                                            case 'all':
-                                                $sql = "select * from tbl_product";
-                                                break;
-                                            case 'b-a-z':
-                                                $sql = "select * from tbl_product order by brand_id asc";
-                                                break;
-                                            case 'b-z-a':
-                                                $sql = "select * from tbl_product order by brand_id desc";
-                                                break;
-                                            case 'c-a-z':
-                                                $sql = "select * from tbl_product order by cate_id asc";
-                                                break;
-                                            case 'c-z-a':
-                                                $sql = "select * from tbl_product order by cate_id desc";
-                                                break;
-                                            case 'l-h':
-                                                $sql = "select * from tbl_product order by prod_price asc";
-                                                break;
-                                            case 'h-l':
-                                                $sql = "select * from tbl_product order by prod_price desc";
-                                                break;
-                                        }
-                                    }
-                                    $sql .= " limit 12";
-                                    $result = mysqli_query($conn, $sql);
                                     while ($row = mysqli_fetch_array($result)) {
                                     ?>
 
@@ -103,13 +100,12 @@
                                                     <a href="index.php?p=single-product&id=<?= $row['prod_id'] ?>">
                                                         <img src="images/products/<?= $row['prod_img'] ?>" alt="Li's Product Image" style="width: auto; height:150px; object-fit: cover; object-position: center;">
                                                     </a>
-                                                    <span class="sticker">New</span>
                                                 </div>
                                                 <div class="product_desc">
                                                     <div class="product_desc_info">
                                                         <div class="product-review">
                                                             <h5 class="manufacturer">
-                                                                <a href="index.php?p=single-product&id=<?= $row['prod_id'] ?>">Graphic Corner</a>
+                                                                <a href="index.php?p=single-product&id=<?= $row['prod_id'] ?>"><?= $row['cate_name'] ?></a>
                                                             </h5>
                                                             <div class="rating-box">
                                                                 <ul class="rating">
@@ -138,47 +134,6 @@
                                         </div>
                                     <?php }
                                     ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="list-view" class="tab-pane product-list-view fade" role="tabpanel">
-                            <div class="row">
-                                <div class="col">
-                                    <div class="row product-layout-list">
-                                        <div class="col-lg-3 col-md-5 ">
-                                            <div class="product-image">
-                                                <a href="single-product.html">
-                                                    <img src="images/product/large-size/12.jpg" alt="Li's Product Image">
-                                                </a>
-                                                <span class="sticker">New</span>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-5 col-md-7">
-                                            <div class="product_desc">
-                                                <div class="product_desc_info">
-                                                    <div class="product-review">
-                                                        <h5 class="manufacturer">
-                                                            <a href="product-details.html">Graphic Corner</a>
-                                                        </h5>
-                                                        <div class="rating-box">
-                                                            <ul class="rating">
-                                                                <li><i class="fa fa-star-o"></i></li>
-                                                                <li><i class="fa fa-star-o"></i></li>
-                                                                <li><i class="fa fa-star-o"></i></li>
-                                                                <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                                                <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                    <h4><a class="product_name" href="single-product.html">Hummingbird printed t-shirt</a></h4>
-                                                    <div class="price-box">
-                                                        <span class="new-price">$46.80</span>
-                                                    </div>
-                                                    <p>Beach Camera Exclusive Bundle - Includes Two Samsung Radiant 360 R3 Wi-Fi Bluetooth Speakers. Fill The Entire Room With Exquisite Sound via Ring Radiator Technology. Stream And Control R3 Speakers Wirelessly With Your Smartphone. Sophisticated, Modern Desig</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>

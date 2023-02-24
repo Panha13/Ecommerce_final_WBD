1892c7eb6fcf8c73f96daf92f7c8a7ed24beafa8
devit2000 <ditway1405@gmail.com>
2023-02-24 21:54:43 +0700

    we're on progress boyssssssssssssssssssss

--- shop.php
+++ shop.php
@@ -40,66 +40,104 @@
                     <div class="product-select-box">
                         <div class="product-short">
                             <p>Sort By:</p>
-                            <select class="nice-select">
-                                <option value="trending">Relevance</option>
-                                <option value="sales">Name (A - Z)</option>
-                                <option value="sales">Name (Z - A)</option>
-                                <option value="rating">Price (Low &gt; High)</option>
-                                <option value="date">Rating (Lowest)</option>
-                                <option value="price-asc">Model (A - Z)</option>
-                                <option value="price-asc">Model (Z - A)</option>
-                            </select>
+                            <form action="index.php?p=shop" method='POST'>
+                                <select class="nice-select" name="choice">
+                                    <option value="all" selected>Relevance</option>
+                                    <option value="b-a-z">Brand (A - Z)</option>
+                                    <option value="b-z-a">Brand (Z - A)</option>
+                                    <option value="l-h">Price (Low &gt; High)</option>
+                                    <option value="c-a-z">Category (A - Z)</option>
+                                    <option value="c-z-a">Category (Z - A)</option>
+                                </select>
+                                <button>Go</button>
+                            </form>
                         </div>
                     </div>
                     <!-- product-select-box end -->
                 </div>
                 <!-- shop-top-bar end -->
-                <!-- shop-products-wrapper start -->
-                <div class="shop-products-wrapper">
+                <!-- shop-product-wrapper start -->
+                <div class="shop-product-wrapper">
                     <div class="tab-content">
                         <div id="grid-view" class="tab-pane fade active show" role="tabpanel">
                             <div class="product-area shop-product-area">
                                 <div class="row">
-                                    <div class="col-lg-4 col-md-4 col-sm-6 mt-40">
-                                        <!-- single-product-wrap start -->
-                                        <div class="single-product-wrap">
-                                            <div class="product-image">
-                                                <a href="single-product.html">
-                                                    <img src="images/product/large-size/1.jpg" alt="Li's Product Image">
-                                                </a>
-                                                <span class="sticker">New</span>
-                                            </div>
-                                            <div class="product_desc">
-                                                <div class="product_desc_info">
-                                                    <div class="product-review">
-                                                        <h5 class="manufacturer">
-                                                            <a href="product-details.html">Graphic Corner</a>
-                                                        </h5>
-                                                        <div class="rating-box">
-                                                            <ul class="rating">
-                                                                <li><i class="fa fa-star-o"></i></li>
-                                                                <li><i class="fa fa-star-o"></i></li>
-                                                                <li><i class="fa fa-star-o"></i></li>
-                                                                <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                                                <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                                            </ul>
+                                    <?php
+                                    $sql = "all";
+                                    $sql = "select * from tbl_product";
+                                    if (isset($_POST['choice'])) {
+                                        $c = $_POST['choice'];
+                                        switch ($c) {
+                                            case 'all':
+                                                $sql = "select * from tbl_product";
+                                                break;
+                                            case 'b-a-z':
+                                                $sql = "select * from tbl_product order by brand_id asc";
+                                                break;
+                                            case 'b-z-a':
+                                                $sql = "select * from tbl_product order by brand_id desc";
+                                                break;
+                                            case 'c-a-z':
+                                                $sql = "select * from tbl_product order by cate_id asc";
+                                                break;
+                                            case 'c-z-a':
+                                                $sql = "select * from tbl_product order by cate_id desc";
+                                                break;
+                                            case 'l-h':
+                                                $sql = "select * from tbl_product order by prod_price asc";
+                                                break;
+                                            case 'h-l':
+                                                $sql = "select * from tbl_product order by prod_price desc";
+                                                break;
+                                        }
+                                    }
+                                    $sql .= " limit 12";
+                                    $result = mysqli_query($conn, $sql);
+                                    while ($row = mysqli_fetch_array($result)) {
+                                    ?>
+
+                                        <div class="col-lg-4 col-md-4 col-sm-6 mt-40">
+                                            <!-- single-product-wrap start -->
+                                            <div class="single-product-wrap">
+                                                <div class="product-image d-flex" style="justify-content: center;">
+                                                    <a href="index.php?p=single-product&id=<?= $row['prod_id'] ?>">
+                                                        <img src="images/products/<?= $row['prod_img'] ?>" alt="Li's Product Image" style="width: auto; height:150px; object-fit: cover; object-position: center;">
+                                                    </a>
+                                                    <span class="sticker">New</span>
+                                                </div>
+                                                <div class="product_desc">
+                                                    <div class="product_desc_info">
+                                                        <div class="product-review">
+                                                            <h5 class="manufacturer">
+                                                                <a href="index.php?p=single-product&id=<?= $row['prod_id'] ?>">Graphic Corner</a>
+                                                            </h5>
+                                                            <div class="rating-box">
+                                                                <ul class="rating">
+                                                                    <li><i class="fa fa-star-o"></i></li>
+                                                                    <li><i class="fa fa-star-o"></i></li>
+                                                                    <li><i class="fa fa-star-o"></i></li>
+                                                                    <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                                    <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                        <h4><a class="product_name" href="index.php?p=single-product&id=<?= $row['prod_id'] ?>"><?= $row['prod_name'] ?></a></h4>
+                                                        <div class="price-box">
+                                                            <span class="new-price">$<?= $row['prod_price'] ?></span>
                                                         </div>
                                                     </div>
-                                                    <h4><a class="product_name" href="single-product.html">Accusantium dolorem1</a></h4>
-                                                    <div class="price-box">
-                                                        <span class="new-price">$46.80</span>
+                                                    <div class="add-actions">
+                                                        <ul class="add-actions-link">
+                                                            <li class="add-cart active"><a href="shopping-cart.html">Add to cart</a></li>
+                                                            <li><a href="#" title="quick view" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li>
+                                                            <li><a class="links-details" href="wishlist.html"><i class="fa fa-heart-o"></i></a></li>
+                                                        </ul>
                                                     </div>
-                                                </div>
-                                                <div class="add-actions">
-                                                    <ul class="add-actions-link">
-                                                        <li class="add-cart active"><a href="shopping-cart.html">Add to cart</a></li>
-                                                        <li><a href="#" title="quick view" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li>
-                                                        <li><a class="links-details" href="wishlist.html"><i class="fa fa-heart-o"></i></a></li>
-                                                    </ul>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    <?php }
+                                    ?>
                                 </div>
                             </div>
                         </div>
